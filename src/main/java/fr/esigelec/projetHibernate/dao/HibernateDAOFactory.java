eec858b82ddbda713d360f403f95b59c2869a77b
LirycMoaners <cyril.lefebvre.27@gmail.com>
2015-11-25 18:04:18 +0100

    Compte rendu encore

--- src/main/java/fr/esigelec/projetHibernate/dao/HibernateDAOFactory.java
+++ src/main/java/fr/esigelec/projetHibernate/dao/HibernateDAOFactory.java
@@ -4,16 +4,12 @@ import fr.esigelec.projetHibernate.dao.hibernate.PaysDAOImpl;
 import fr.esigelec.projetHibernate.dao.hibernate.VilleDAOImpl;
 
 public class HibernateDAOFactory extends DAOFactory{
-
 	@Override
 	public IPaysDAO getPaysDAO() {
 		return new PaysDAOImpl();
 	}
-
 	@Override
 	public IVilleDAO getVilleDAO() {
 		return new VilleDAOImpl();
 	}
-
-	
 }
--- src/main/java/fr/esigelec/projetHibernate/dao/JDBCDAOFactory.java
+++ src/main/java/fr/esigelec/projetHibernate/dao/JDBCDAOFactory.java
@@ -4,15 +4,12 @@ import fr.esigelec.projetHibernate.dao.jdbc.PaysDAO;
 import fr.esigelec.projetHibernate.dao.jdbc.VilleDAO;
 
 public class JDBCDAOFactory extends DAOFactory{
-
 	@Override
 	public IPaysDAO getPaysDAO() {
 		return new PaysDAO();
 	}
-
 	@Override
 	public IVilleDAO getVilleDAO() {
 		return new VilleDAO();
 	}
-
 }
